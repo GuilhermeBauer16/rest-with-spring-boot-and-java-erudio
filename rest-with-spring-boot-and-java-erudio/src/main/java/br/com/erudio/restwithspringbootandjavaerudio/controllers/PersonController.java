ca64dff3668289388f2456e538138f1695a72ef1
GuilhermeBauer16 <guilhemebauer16@gmail.com>
2024-01-22 22:30:11 -0300

    feat: created product controller

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/restwithspringbootandjavaerudio/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/restwithspringbootandjavaerudio/controllers/PersonController.java
@@ -4,6 +4,7 @@ import br.com.erudio.restwithspringbootandjavaerudio.model.PersonModel;
 import br.com.erudio.restwithspringbootandjavaerudio.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,22 +15,19 @@ import java.util.List;
 public class PersonController {
     @Autowired
     private PersonServices personServices;
-    @RequestMapping(value = "/{id}",
-            method = RequestMethod.GET,
+    @GetMapping(value = "/{id}",
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public PersonModel findById(@PathVariable(value = "id") String id) {
+    public PersonModel findById(@PathVariable(value = "id") Long id) {
 
         return personServices.findById(id);
     }
 
-    @RequestMapping(method = RequestMethod.GET,
-    produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<PersonModel> findAll(){
         return personServices.findAll();
     }
 
-    @PostMapping(
-            value = "/create",
+    @PostMapping(value = "/create",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public PersonModel create(@RequestBody PersonModel personModel) {
@@ -37,8 +35,7 @@ public class PersonController {
         return personServices.create(personModel);
     }
 
-    @PutMapping(
-            value = "/update",
+    @PutMapping(value = "/update",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public PersonModel update(@RequestBody PersonModel personModel) {
@@ -47,8 +44,10 @@ public class PersonController {
     }
 
     @DeleteMapping(value = "/{id}")
-    public void delete(@PathVariable(value = "id") String id) {
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+
         personServices.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 
--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/restwithspringbootandjavaerudio/services/PersonServices.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/restwithspringbootandjavaerudio/services/PersonServices.java
@@ -1,66 +1,59 @@
 package br.com.erudio.restwithspringbootandjavaerudio.services;
 
 import br.com.erudio.restwithspringbootandjavaerudio.model.PersonModel;
+import br.com.erudio.restwithspringbootandjavaerudio.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
+import java.lang.module.ResolutionException;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 @Service
 public class PersonServices {
+    private final Logger logger = Logger.getLogger(PersonServices.class.getName());
 
-    private final AtomicLong counter = new AtomicLong();
-    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+    @Autowired
+    private PersonRepository repository;
 
     public List<PersonModel> findAll() {
-        List<PersonModel> people = new ArrayList<>();
-        for (int i = 0; i < 8; i++) {
-            PersonModel person = mockPerson(i);
-            people.add(person);
 
-        }
-        return people;
+        return repository.findAll();
     }
 
-    public PersonModel create(PersonModel personModel){
+    public PersonModel create(PersonModel personModel) {
         logger.info("Creating one person");
-        return personModel;
+        return repository.save(personModel);
     }
 
-    public void delete(String id){
+    public void delete(Long id) {
         logger.info("Deleting one person");
 
+        PersonModel entity = repository.findById(id)
+                .orElseThrow(() -> new ResolutionException("No records found for this ID!"));
+
+        repository.delete(entity);
+
+
     }
 
-    public PersonModel update(PersonModel personModel){
+    public PersonModel update(PersonModel personModel) {
+
         logger.info("Updating one person");
-        return personModel;
+        PersonModel entity = repository.findById(personModel.getId())
+                .orElseThrow(() -> new ResolutionException("No records found for this ID!"));
+        entity.setFirstName(personModel.getFirstName());
+        entity.setLastName(personModel.getLastName());
+        entity.setAddress(personModel.getAddress());
+        entity.setGender(personModel.getGender());
+
+        return repository.save(personModel);
     }
 
 
-
-    public PersonModel findById(String id) {
-        logger.info("Finding one person");
-        PersonModel person = new PersonModel();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("Guilherme");
-        person.setLastName("Bauer");
-        person.setAddress("Rio grande do sul");
-        person.setGender("Male");
-        return person;
+    public PersonModel findById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResolutionException("No records found for this ID"));
     }
 
-    private PersonModel mockPerson(int i) {
-        logger.info("Finding all people");
-        PersonModel person = new PersonModel();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("first name: " + i);
-        person.setLastName("last name: " + i);
-        person.setAddress("some address in Brazil: " + i);
-        person.setGender("Male");
-        return person;
-    }
 
 }
